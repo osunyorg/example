63b8503773ecd40d2cd1400a799944e12040c128
alexisben <alexiben7@gmail.com>
2022-01-17 14:57:21 +0100

    Add linters and postcss config

---
+++ postcss.config.js
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    plugins: {
+        '@fullhuman/postcss-purgecss': {
+            content: ['./themes/**/*.html'],
+            safelist: [
+                'show',
+                'active',
+                'collapsed',
+                /^is-/,
+                /^has-/,
+                /^js-/
+            ]
+        },
+        autoprefixer: {},
+        cssnano: {
+            preset: 'default'
+        }
+    }
+};
