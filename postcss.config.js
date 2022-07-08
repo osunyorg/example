04a7f7595812518d8dd17081f4464e632e32f9ca
alexisben <alexiben7@gmail.com>
2022-07-08 11:48:10 +0200

    combination of html partials and hugo stats

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,75 @@
 /* eslint-disable no-undef */
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    content: ['./hugo_stats.json'],
-    defaultExtractor: content => {
-        const els = JSON.parse(content).htmlElements;
-        return [
-            ...(els.tags || []),
-            ...(els.classes || []),
-            ...(els.ids || [])
-        ];
-    },
-    safelist: []
-});
+const purgecssConfig = {
+    content: [
+        './hugo_stats.json',
+        './themes/**/*.html',
+        'layouts/**/*.html'
+    ],
+    extractors: [
+        {
+            extensions: ['json'],
+            extractor: (content) => {
+                const els = JSON.parse(content).htmlElements;
+                return [
+                    ...(els.tags || []),
+                    ...(els.classes || []),
+                    ...(els.ids || [])
+                ];
+            }
+        }
+    ],
+    safelist: {
+        standard: [
+            'show',
+            'active',
+            'collapsed',
+            /^dropdown/,
+            /^nav-level-/,
+            /^splide_/,
+            /^is-/,
+            /^has-/,
+            /^js-/,
+
+            // Glightbox
+            'wait-autoplay',
+            'gfadeIn',
+            'gfadeOut',
+            'gslideOutLeft',
+            'gslideInLeft',
+            'gslideOutRight',
+            'gslideInRight',
+            'gzoomIn',
+            'gzoomOut'
+        ],
+        deep: [
+            // Glightbox
+            /ratio/,
+            /^glightbox/,
+            /^gslide/,
+            /^desc-top/,
+            /^desc-left/,
+            /^ginlined/,
+            /^zoomed/,
+            /^gdesc-/,
+            /^gabsolute/,
+            /^grelative/,
+            /^gloader/,
+            /^goverlay/,
+            /^gprev/,
+            /^gnext/,
+            /^gclose/,
+            /^gbtn/,
+            /^gcontainer/
+        ]
+    }
+};
 
 module.exports = {
-    plugins: [[purgecss]]
+    plugins: {
+        autoprefixer: {},
+        cssnano: {
+            preset: 'default'
+        },
+        '@fullhuman/postcss-purgecss': purgecssConfig
+    }
 };
