7988eeeadd82e01afdabd800df82ff0c510cdaff
alexisben <alex@noesya.coop>
2023-12-29 12:24:02 +0100

    test lost pixel

---
+++ lostpixel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pageShots: {
+    pages: [ { path: '/', name: 'landing' } ],
+    baseUrl: 'http://localhost:1313',
+  },
+  lostPixelProjectId: 'clqqjeyp939ddog0eqza6pdwg',
+  apiKey: process.env.LOST_PIXEL_API_KEY,
+};
