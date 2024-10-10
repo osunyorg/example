5511583016af779ecb8f722fde19c321430059d9
Clara Rigaud <clararigaud0@gmail.com>
2024-10-10 14:49:27 +0200

    theme with slider and lightbox

--- assets/js/preview.js
+++ assets/js/preview.js
@@ -1,11 +1,11 @@
-import './vendors/lightbox';
+import './theme/components/lightbox';
 import './theme/components/carousel';
 import './theme/utils/a11y';
 import './theme/utils/breakpoints';
 import './theme/utils/isReducedMotionPrefered';
 import './theme/utils/number';
 import './theme/design-system/notes';
-import './theme/blocks/locations';
+// import './theme/blocks/locations';
 import './theme/blocks/draggableBlocks';
 import './theme/blocks/keyFigures';
 import './theme/blocks/organizations';
