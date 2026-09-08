1ca86a3cadbd1293d5543234d2d8ab852948a523
Alexis BENOIT <alex@noesya.coop>
2026-09-08 22:08:15 +0200

    try purgecss

---
+++ purgecss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    'public/**/*.js',
+    'public/**/*.html',
+    'public/index.html'
+  ],
+  css: [
+    'public/assets/css/main.*.css'
+  ],
+  output: 'public/assets/css/'
+}
